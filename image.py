95b9624d08d976b4e755707fe1b21cacd551f3fd
CHENGB <ben.cheng@mpi.govt.nz>
2023-12-14 12:24:09 +1300

    feat: added python scripts to update image on inky

---
+++ image.py
@@ -0,0 +1,21 @@
+import sys
+from inky.auto import auto
+from PIL import Image
+
+inky = auto(ask_user=True, verbose=True)
+saturation = 0.5
+
+if len(sys.argv) == 1:
+    print("""
+Usage: {file} image-file
+""".format(file=sys.argv[0]))
+    sys.exit(1)
+
+image = Image.open(sys.argv[1])
+resizedimage = image.resize(inky.resolution)
+
+if len(sys.argv) > 2:
+    saturation = float(sys.argv[2])
+
+inky.set_image(resizedimage, saturation=saturation)
+inky.show()
