d57c7cfae0f244fdefe19a95fec90c8aa0c2d00d
CHENGB <ben.cheng@mpi.govt.nz>
2023-12-14 19:12:00 +1300

    feat: display on inky

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from image import display
+import time
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "./uploads"
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    if request.method == 'POST':
+        display(request.files['file'])
+        # time.sleep(1)
+        return redirect(request.referrer)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
--- image.py
+++ image.py
@@ -1,21 +1,9 @@
-import sys
 from inky.auto import auto
 from PIL import Image
 
-inky = auto(ask_user=True, verbose=True)
-saturation = 0.5
 
-if len(sys.argv) == 1:
-    print("""
-Usage: {file} image-file
-""".format(file=sys.argv[0]))
-    sys.exit(1)
-
-image = Image.open(sys.argv[1])
-resizedimage = image.resize(inky.resolution)
-
-if len(sys.argv) > 2:
-    saturation = float(sys.argv[2])
-
-inky.set_image(resizedimage, saturation=saturation)
-inky.show()
+def display(image):
+    inky = auto(ask_user=True, verbose=True)
+    resized_image = Image.open(image).resize(inky.resolution)
+    inky.set_image(resized_image, saturation=0.5)
+    inky.show()
