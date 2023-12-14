7e8e352f170e2071e8344ec64473ec455a14fb55
Ben Cheng <chenghopan@gmail.com>
2023-12-14 19:37:10 +1300

    faet: added port

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request, redirect
 from image import display
-import time
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "./uploads"
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+
     if request.method == 'GET':
         return render_template("index.html")
 
     if request.method == 'POST':
         display(request.files['file'])
-        # time.sleep(1)
         return redirect(request.referrer)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=3000)
