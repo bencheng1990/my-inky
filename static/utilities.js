3a42c359b5ffd047392453d2a691c961a1d0e6d2
CHENGB <ben.cheng@mpi.govt.nz>
2023-12-14 19:12:00 +1300

    feat: display on inky

---
+++ static/utilities.js
@@ -0,0 +1,19 @@
+function displaySelectedImage(event, elementId) {
+    const selectedImage = document.getElementById(elementId);
+    const fileInput = event.target;
+
+    if (fileInput.files && fileInput.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            selectedImage.src = e.target.result;
+        };
+
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+function showSending(elementId) {
+  const button= document.getElementById(elementId);
+  button.value = 'Sending to Inky..';
+}
